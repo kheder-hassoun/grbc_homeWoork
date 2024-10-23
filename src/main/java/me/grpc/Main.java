70d7e574f46d463f84868954524e4d0591d2ba28
kheder <khdrhswn32@gmail.com>
2024-10-23 18:11:44 +0300

    Json format library,

--- src/main/java/me/grpc/Main.java
+++ src/main/java/me/grpc/Main.java
@@ -1,137 +1,92 @@
 package me.grpc;
 
-import com.google.gson.Gson;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidProtocolBufferException, IOException {
+        // Serialize to JSON
+        String jsonMsg = senderToJson();
+        System.out.println("Serialized JSON Message:");
+        System.out.println(jsonMsg);
 
-        String jsonMsg = sender();
-
-        System.out.println("Size of serialized JSON message: " + jsonMsg.getBytes().length + " bytes");
-
-        receiver(jsonMsg);
+        // Deserialize from JSON
+        receiverFromJson(jsonMsg);
     }
 
     //-----------------------------------------------------------------------------------
-    private static String sender() {
-        JsonMenuItem item1 = new JsonMenuItem("Item 1", "item 1 bla bla bla", 12.99f, "MAIN_COURSE", true);
-        JsonMenuItem item2 = new JsonMenuItem("item 2", "item 2 bla bla bla", 5.99f, "DESSERT", true);
+    private static String senderToJson() throws IOException {
+        MenuItem item1 = MenuItem.newBuilder()
+                .setName("Item 1 ")
+                .setDescription(" item 1 bla bla bla ")
+                .setPrice(12.99f)
+                .setCategory(Category.MAIN_COURSE)
+                .setAvailable(true)
+                .build();
 
-        JsonRestaurant restaurant1 = new JsonRestaurant("kheder restaurant", "123 jableh city", "123-456-7890", new JsonMenuItem[]{item1, item2});
+        MenuItem item2 = MenuItem.newBuilder()
+                .setName("item 2")
+                .setDescription("item 2 bla bla bla ")
+                .setPrice(5.99f)
+                .setCategory(Category.DESSERT)
+                .setAvailable(true)
+                .build();
 
-        JsonMenuItem item3 = new JsonMenuItem("mateh", "mateh description", 2.50f, "BEVERAGE", true);
+        Restaurant restaurant1 = Restaurant.newBuilder()
+                .setName("kheder resturnat")
+                .setLocation("123 jableh city")
+                .setContactNumber("123-456-7890")
+                .addMenu(item1)
+                .addMenu(item2)
+                .build();
 
-        JsonRestaurant restaurant2 = new JsonRestaurant("hadeel restaurant", "456 latakia", "555-123-4567", new JsonMenuItem[]{item3});
+        MenuItem item3 = MenuItem.newBuilder()
+                .setName("mateh ")
+                .setDescription("mateh descrption")
+                .setPrice(2.50f)
+                .setCategory(Category.BEVERAGE)
+                .setAvailable(true)
+                .build();
 
-        JsonRestaurantList restaurantList = new JsonRestaurantList(new JsonRestaurant[]{restaurant1, restaurant2});
+        Restaurant restaurant2 = Restaurant.newBuilder()
+                .setName("hadeel resturant")
+                .setLocation("456 latakia ")
+                .setContactNumber("555-123-4567")
+                .addMenu(item3)
+                .build();
 
-        Gson gson = new Gson();
-        String jsonMsg = gson.toJson(restaurantList);
+        RestaurantList restaurantList = RestaurantList.newBuilder()
+                .addRestaurants(restaurant1)
+                .addRestaurants(restaurant2)
+                .build();
 
-        System.out.println("Serialized JSON Message:\n" + jsonMsg);
+        String jsonMsg = JsonFormat.printer().print(restaurantList);
 
         return jsonMsg;
     }
 
-    private static void receiver(String jsonMsg) {
-        Gson gson = new Gson();
-        JsonRestaurantList restaurantList = gson.fromJson(jsonMsg, JsonRestaurantList.class);
+    //-----------------------------------------------------------------------------------
+    private static void receiverFromJson(String jsonMsg) throws InvalidProtocolBufferException, IOException {
+        System.out.println("\nReceived JSON Message:");
+        System.out.println(jsonMsg);
 
-        System.out.println("\nDeserialized Restaurant List:");
-        for (JsonRestaurant restaurant : restaurantList.getRestaurants()) {
+        RestaurantList.Builder restaurantListBuilder = RestaurantList.newBuilder();
+        JsonFormat.parser().merge(jsonMsg, restaurantListBuilder);
+        RestaurantList restaurantList = restaurantListBuilder.build();
+
+        System.out.println("Deserialized Restaurant List:");
+        for (Restaurant restaurant : restaurantList.getRestaurantsList()) {
             System.out.println("Restaurant Name: " + restaurant.getName());
             System.out.println("Location: " + restaurant.getLocation());
             System.out.println("Contact Number: " + restaurant.getContactNumber());
 
             System.out.println("Menu:");
-            for (JsonMenuItem item : restaurant.getMenu()) {
+            for (MenuItem item : restaurant.getMenuList()) {
                 System.out.println("- " + item.getName() + ": " + item.getDescription() +
                         " ($" + item.getPrice() + ") [Category: " + item.getCategory() + "]");
             }
             System.out.println();
         }
-    }
-}
-
-// Supporting classes for JSON serialization with new names to avoid conflict
-class JsonMenuItem {
-    private String name;
-    private String description;
-    private float price;
-    private String category;
-    private boolean available;
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public float getPrice() {
-        return price;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public boolean isAvailable() {
-        return available;
-    }
-
-    public JsonMenuItem(String name, String description, float price, String category, boolean available) {
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.category = category;
-        this.available = available;
-    }
-
-    // Getters and setters (optional)...
-}
-
-class JsonRestaurant {
-    private String name;
-    private String location;
-    private String contactNumber;
-    private JsonMenuItem[] menu;
-
-    public String getName() {
-        return name;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public String getContactNumber() {
-        return contactNumber;
-    }
-
-    public JsonMenuItem[] getMenu() {
-        return menu;
-    }
-
-    public JsonRestaurant(String name, String location, String contactNumber, JsonMenuItem[] menu) {
-        this.name = name;
-        this.location = location;
-        this.contactNumber = contactNumber;
-        this.menu = menu;
-    }
-
-    // Getters and setters (optional)...
-}
-
-class JsonRestaurantList {
-    private JsonRestaurant[] restaurants;
-
-    public JsonRestaurantList(JsonRestaurant[] restaurants) {
-        this.restaurants = restaurants;
-    }
-
-    public JsonRestaurant[] getRestaurants() {
-        return restaurants;
     }
 }
